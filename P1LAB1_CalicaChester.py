4e6fa1bf991e32c0d04576e73455b10869ffa27a
CaliCodez <80232267+CaliCodez@users.noreply.github.com>
2021-03-07 21:23:47 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_CalicaChester.py
@@ -0,0 +1,4 @@
+userNum = int(input())              # CaliCodez
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
