9a07f129eb33421492f7cf8e385e053938c12afa
CaliCodez <80232267+CaliCodez@users.noreply.github.com>
2021-03-07 21:31:05 -0500

    Delete P1LAB1_CalicaChester.py

--- P1LAB1_CalicaChester.py
+++
@@ -1,4 +0,0 @@
-userNum = int(input())              # CaliCodez
-userNumSquared = userNum * userNum   # Bug here; fix it when instructed
-   
-print(userNumSquared)       # Output formatting issue here; fix it when instructed
